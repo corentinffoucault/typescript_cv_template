12779f5ce84f808924d8f7b96ef8d9290e4eadeb
corentin <corentinf@free.fr>
2025-07-28 13:17:33 +0200

    initialisation

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Resume from "./resume"
+
+function main() {
+    console.log(new Resume().print())
+}
+
+void main()
---
+++ src/resume.ts
@@ -0,0 +1,5 @@
+export default class Resume {
+    public print() {
+        return "hello world";
+    }
+}
