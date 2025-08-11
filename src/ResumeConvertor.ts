8664fc7af78dc52801790430985547b393bb43e2
corentin <corentinf@free.fr>
2025-08-11 13:52:53 +0200

    ajout d'interface global

--- src/ResumeConvertor.ts
+++ src/ResumeConvertor.ts
@@ -4,8 +4,9 @@ import { fileURLToPath } from 'url';
 
 import { ResumeGenerator } from "./components/resume.js";
 import { ResumeSchema } from "../../json_cv_schema/src/type/type.js";
+import { IResumeConvertor } from "../../json_cv_schema/src/type/IResumeConvertor.js";
 
-export default class ResumeConvertor {
+export default class ResumeConvertor implements IResumeConvertor {
     public async generateResume(resumeJson: ResumeSchema) {
         const __filename = fileURLToPath(import.meta.url);
         const _srcPath = path.dirname(__filename);
